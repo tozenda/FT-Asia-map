812a8d4903b48dd7b44de802d5513639740ff25e
Thomas OZENDA <thomas.ozenda@viseo.com>
2021-08-09 14:46:22 +0800

    Added CSV parsing and added communities on map

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,23 @@
 var mymap = L.map('map', { zoomControl: false }).setView([6.92, 115.06], 3);
 
+const addContactToMap = (contacts) => {
+    contacts.forEach((ft) => {
+        if (ft) {
+            console.log("Adding ", ft.name, ft.lat, ft.lon);
+            L.marker([ft.lat, ft.lon]).addTo(mymap).bindPopup(ft.name);
+        }
+    });
+};
+
+Papa.parse('./data/ft-contact.csv', {
+    header: true,
+    download: true,
+    dynamicTyping: true,
+    complete: function(results) {
+        addContactToMap(results.data);
+    }
+});
+
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/512/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
@@ -8,5 +26,3 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/512/{z}/{x}/{y}?access_
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoidG96ZW5kYSIsImEiOiJja3JhZXE4bmI0ZjJ1Mm5xaG42ZHJzMHlyIn0.08JChHjF6oRgd7PhkbiWwQ'
 }).addTo(mymap);
-
-var marker = L.marker([1.352083, 103.819836]).addTo(mymap);
